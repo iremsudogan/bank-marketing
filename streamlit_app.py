ff050c23c650df8331a964aa268c198a53a2cabd
iremsudogan <108758842+iremsudogan@users.noreply.github.com>
2024-05-31 13:19:32 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,6 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import LabelEncoder
 from sklearn.compose import ColumnTransformer
 
-# Modelin yüklenmesi
-loaded_model = pickle.load(open('bank_marketing_prediction.sav', 'rb'))
-
-# Kategorik sütunlar için özel etiketleyici dönüştürücü
 class CustomLabelEncoder(BaseEstimator, TransformerMixin):
     def _init_(self, columns):
         self.columns = columns
@@ -42,52 +38,52 @@ class FeaturesAdder(BaseEstimator, TransformerMixin):
         X_new['age_education_level'] = X_new['age'] * X_new['education'].astype('category').cat.codes
         return X_new
 
+# Load the model outside the function to avoid re-loading it every time the function is called
+loaded_model = pickle.load(open('bank_marketing_prediction.sav', 'rb'))
+
 def bank_marketing_prediction(input_data):
-    # Özellik isimlerini eğitim verileriyle eşleştirin
+    # Correct the feature names to match the training data
     column_names = ['age', 'job', 'marital', 'education', 'default', 'housing', 'loan',
                     'contact', 'month', 'day_of_week', 'campaign', 'pdays', 'previous', 
                     'poutcome', 'emp.var.rate', 'cons.price.idx', 'cons.conf.idx', 
-                    'euribor3m', 'nr.employed']  # Alt çizgi yerine nokta kullanın
+                    'euribor3m', 'nr.employed']
 
-    input_df = pd.DataFrame([input_data], columns=column_names)
-    prediction = loaded_model.predict(input_df)
+    input_data = pd.DataFrame([input_data], columns=column_names)
+    prediction = loaded_model.predict(input_data)
     return prediction[0]
 
-
 def main():
-    # Başlık
-    st.title('Banka Pazarlama Tahmin Web Uygulaması')
+    # Giving a title
+    st.title('Bank Marketing Prediction Web App')
     
-    # Kullanıcıdan veri alma
-    age = st.number_input('Yaş')
-    job = st.selectbox('Meslek', ['admin.', 'blue-collar', 'entrepreneur', 'housemaid', 'management', 
+    # Getting input from the user
+    age = st.number_input('Age')
+    job = st.selectbox('Job', ['admin.', 'blue-collar', 'entrepreneur', 'housemaid', 'management', 
                                'retired', 'self-employed', 'services', 'student', 'technician', 
                                'unemployed', 'unknown'])
-    marital = st.selectbox('Medeni Durum', ['divorced', 'married', 'single', 'unknown'])
-    education = st.selectbox('Eğitim Seviyesi', ['basic.4y', 'basic.6y', 'basic.9y', 'high.school', 
+    marital = st.selectbox('Marital Status', ['divorced', 'married', 'single', 'unknown'])
+    education = st.selectbox('Education Level', ['basic.4y', 'basic.6y', 'basic.9y', 'high.school', 
                                                  'illiterate', 'professional.course', 'university.degree', 'unknown'])
-    default = st.selectbox('Kredi Temerrüdü', ['no', 'yes', 'unknown'])
-    housing = st.selectbox('Konut Kredisi', ['no', 'yes', 'unknown'])
-    loan = st.selectbox('Bireysel Kredi', ['no', 'yes', 'unknown'])
-    contact = st.selectbox('İletişim Türü', ['cellular', 'telephone'])
-    month = st.selectbox('Son İletişim Ayı', ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 
+    default = st.selectbox('Credit in Default', ['no', 'yes', 'unknown'])
+    housing = st.selectbox('Housing Loan', ['no', 'yes', 'unknown'])
+    loan = st.selectbox('Personal Loan', ['no', 'yes', 'unknown'])
+    contact = st.selectbox('Contact Communication Type', ['cellular', 'telephone'])
+    month = st.selectbox('Last Contact Month', ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 
                                                 'aug', 'sep', 'oct', 'nov', 'dec'])
-    day_of_week = st.selectbox('Son İletişim Günü', ['mon', 'tue', 'wed', 'thu', 'fri'])
-    campaign = st.number_input('Bu Kampanya Sırasındaki İletişim Sayısı')
-    pdays = st.number_input('Önceki Kampanyadan Sonra Geçen Gün Sayısı')
-    previous = st.number_input('Bu Kampanya Öncesi İletişim Sayısı')
-    poutcome = st.selectbox('Önceki Kampanya Sonucu', ['failure', 'nonexistent', 'success'])
-    emp_var_rate = st.number_input('İstihdam Değişim Oranı')
-    cons_price_idx = st.number_input('Tüketici Fiyat Endeksi')
-    cons_conf_idx = st.number_input('Tüketici Güven Endeksi')
-    euribor3m = st.number_input('Euribor 3 Aylık Faiz Oranı')
-    nr_employed = st.number_input('Çalışan Sayısı')
-
-    # Tahmin için kod
+    day_of_week = st.selectbox('Last Contact Day of Week', ['mon', 'tue', 'wed', 'thu', 'fri'])
+    campaign = st.number_input('Number of Contacts Performed During this Campaign')
+    pdays = st.number_input('Number of Days that Passed by After the Client was Last Contacted from a Previous Campaign')
+    previous = st.number_input('Number of Contacts Performed Before this Campaign')
+    poutcome = st.selectbox('Outcome of the Previous Marketing Campaign', ['failure', 'nonexistent', 'success'])
+    emp_var_rate = st.number_input('Employment Variation Rate')
+    cons_price_idx = st.number_input('Consumer Price Index')
+    cons_conf_idx = st.number_input('Consumer Confidence Index')
+    euribor3m = st.number_input('Euribor 3 Month Rate')
+    nr_employed = st.number_input('Number of Employees')
     prediction = ''
     
-    # Kullanıcıdan alınan giriş verileri
-    if st.button('Banka Pazarlama Tahmini'):
+    # Getting the input data from the user
+    if st.button('Bank Marketing Prediction'):
         prediction = bank_marketing_prediction([age, job, marital, education, default, housing, loan,
                                                 contact, month, day_of_week, campaign, pdays, previous, 
                                                 poutcome, emp_var_rate, cons_price_idx, cons_conf_idx, 
